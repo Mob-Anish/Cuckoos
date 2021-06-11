96976693c41942d3a4260beb132f95f92c29adb6
Mob-Anish <mobanish16@gmail.com>
2021-06-11 20:10:59 +0545

    Cuckoos are ready to clock

---
+++ README.md
@@ -0,0 +1,5 @@
+## Cuckoos
+
+### It is simple clock application using react and used to tell the time and date of the current day. You can see the image 👇👇.
+
+![cuckoos](https://user-images.githubusercontent.com/75711381/121701442-a12d6280-caf0-11eb-9595-533548270c15.PNG)
---
+++ src/containers/App.js
@@ -0,0 +1,112 @@
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.clockling();
+    }, 1000);
+  }
+
+  clockling() {
+    let time;
+    let now = new Date();
+    let sec = now.getSeconds();
+    let min = now.getMinutes();
+    let hour = now.getHours();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    let dy = days[now.getDay()];
+    let date = now.getDate();
+    let month = months[now.getMonth()];
+    let year = now.getFullYear();
+
+    if (hour > 12) {
+      time = "PM";
+      hour = hour - 12;
+    }
+
+    if (hour === 0) {
+      hour = 12;
+    }
+
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+
+    if (min < 10) {
+      min = "0" + min;
+    }
+
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+
+    const tickTock = {
+      day: time,
+      hour,
+      min,
+      sec,
+      dy,
+      date,
+      month,
+      year,
+    };
+
+    const list = [...this.state.list];
+    list.push(tickTock);
+
+    this.setState({
+      list,
+    });
+
+    //console.log(this.state.list[this.state.list.length - 1].sec);
+  }
+
+  render() {
+    return (
+      <div className="clocking">
+        { this.state.list.length >= 1 ?
+          <div>
+            <div className="clock">
+              <span>{this.state.list[this.state.list.length - 1].hour}</span> <span>:</span>
+              <span>{this.state.list[this.state.list.length - 1].min}</span> <span>:</span>
+              <span>
+                {this.state.list[this.state.list.length - 1].sec} {this.state.list[this.state.list.length - 1].day}
+              </span>
+            </div>
+            <div className="date">
+              <span>{this.state.list[this.state.list.length - 1].year}</span> <span>/</span>
+              <span>{this.state.list[this.state.list.length - 1].month}</span> <span>/</span>
+              <span>{this.state.list[this.state.list.length - 1].date}</span> <span>/</span>
+              <span>{this.state.list[this.state.list.length - 1].dy}</span>
+            </div>
+          </div>
+        : ''}
+      </div>
+    );
+  }
+}
+
+export default App;
